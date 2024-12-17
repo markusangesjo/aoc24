6e8cb5238ba967f1fb070c00e1e3d0fd2aa62af3
Markus Angesjö <markusgun@gmail.com>
2024-12-17 22:05:31 +0100

    up

--- Day1/CalcDistance.cs
+++ Day1/CalcDistance.cs
@@ -4,5 +4,32 @@ namespace Day1;
 
 public class CalcDistance
 {
+    public static int Distancediff(string data)
+    {
+         List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            string[] datarows = data.Trim().Split('\n');
+            foreach (string row in datarows)
+            {
+                string num1 = row.Trim().Split(" ",StringSplitOptions.RemoveEmptyEntries)[0];
+                string num2 = row.Trim().Split(" ",StringSplitOptions.RemoveEmptyEntries)[1];
+                
+                int number1 = int.Parse(num1);
+                int number2 = int.Parse(num2);
+                left.Add(number1);
+                right.Add(number2);
+            }
+            left.Sort();
+            right.Sort();
+
+            int sum = 0;
+
+            for (int i = 0; i <left.Count; i++)
+            {
+                int diff = Math.Abs(left[i] - right[i]);
+                sum += diff;
+            }
+        return sum;
+    }
 
 }
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -8,33 +8,8 @@ namespace Day1
         {
             GetInput input = new GetInput();
             string data = input.Readfile();
-
-            List<int> left = new List<int>();
-            List<int> right = new List<int>();
-            string[] datarows = data.Trim().Split('\n');
-            foreach (string row in datarows)
-            {
-                string num1 = row.Trim().Split(" ",StringSplitOptions.RemoveEmptyEntries)[0];
-                string num2 = row.Trim().Split(" ",StringSplitOptions.RemoveEmptyEntries)[1];
-                
-                int number1 = int.Parse(num1);
-                int number2 = int.Parse(num2);
-                left.Add(number1);
-                right.Add(number2);
-            }
-            left.Sort();
-            right.Sort();
-
-            int distancesum = 0;
-
-            for (int i = 0; i <left.Count; i++)
-            {
-                int diff = Math.Abs(left[i] - right[i]);
-                distancesum += diff;
-            }
+            int distancesum = CalcDistance.Distancediff(data);
             Console.WriteLine(distancesum);
-
-
         }
     }
 }
